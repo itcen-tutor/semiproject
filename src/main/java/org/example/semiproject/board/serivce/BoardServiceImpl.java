5bb9786507d17433f1dbe3c404e10335e8f8424b
itcen-tutor <itcen-tutor@itcen-tutor.co.kr>
2025-06-05 15:49:08 +0900

    게시판 기능 구현 - 게시판 페이지네이션 2, 본문글 처리

--- src/main/java/org/example/semiproject/board/serivce/BoardServiceImpl.java
+++ src/main/java/org/example/semiproject/board/serivce/BoardServiceImpl.java
@@ -1,6 +1,7 @@
 package org.example.semiproject.board.serivce;
 
 import lombok.RequiredArgsConstructor;
+import org.example.semiproject.board.domain.Board;
 import org.example.semiproject.board.domain.dto.ListBoardDTO;
 import org.example.semiproject.board.repository.BoardRepository;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,11 @@ public class BoardServiceImpl implements BoardService {
         // cpg에 따라 시작위치값 계산
         int stnum = (cpg - 1) * 25;
         return boardMapper.selectBoard(stnum);
+    }
+
+    @Override
+    public Board readOneBoard(int bno) {
+        return boardMapper.selectOneBoard(bno);
     }
 
 }
